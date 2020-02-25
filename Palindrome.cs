be66a6a1fa8c5baaa377fee044010c806a183d2d
Geoff Goetz and Jamison Cozart <jamison@>
2020-02-25 10:34:46 -0800

    Add reverseString method and compare userString to reverseString

--- Palindrome.cs
+++ Palindrome.cs
@@ -5,10 +5,31 @@ class Palindromes
 {
   public static void Main()
   {
-    //code
+    Console.WriteLine("Please enter a string:");
+    string userString = Console.ReadLine();
+    if(IsPalindrome(userString))
+    {
+      Console.WriteLine("Your string is a Palindrome!");
+    }
+    else{
+      Console.WriteLine("Your string is not a Palindrome");
+    }
   }
-  public bool IsPalindrome()
+  public static bool IsPalindrome(string userString)
   {
-    //check if palindrome
+    if(userString == reverseString(userString))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  public static string reverseString(string forwardString)
+  {
+    char[] charArray = forwardString.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
   }
 }
