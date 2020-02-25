1295ced82495cfd4e006a082cbfda59d7991a4f8
Geoff Goetz and Jamison Cozart <geoff+jamison@>
2020-02-25 10:22:43 -0800

    Add basic file structure

---
+++ Palindrome.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+class Palindromes
+{
+  public static void Main()
+  {
+    //code
+  }
+  public bool IsPalindrome()
+  {
+    //check if palindrome
+  }
+}
